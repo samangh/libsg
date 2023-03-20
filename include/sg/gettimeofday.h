ceb38ca0a103180828bc9e093e7fe5e2f6d77cb4
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2023-03-20 00:17:35 +0000

    Fix error in `gettimeofday()`

--- include/sg/gettimeofday.h
+++ include/sg/gettimeofday.h
@@ -7,7 +7,7 @@
 #ifdef _WIN32
     #include <Winsock2.h>
     #include <sg/extern_c.h>
-    #include <udaq/export/common.h>
+    #include <sg/export/sg_common.h>
 EXTERN_C SG_COMMON_EXPORT int gettimeofday(struct timeval *tp,
                                         struct timezone *tzp);
 #else
