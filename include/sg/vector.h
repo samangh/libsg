63c950dd23fa7dd95c2dd244af368da38b6a1c48
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2023-03-25 19:45:28 +0000

    Made a parameter const

--- include/sg/vector.h
+++ include/sg/vector.h
@@ -1,15 +1,11 @@
 #pragma once
 
-#include <algorithm>
-#include <cstdint>
-#include <memory>
-#include <type_traits>
 #include <vector>
 
 namespace sg::vector {
 
 template <typename T>
-void append(std::vector<T> &base, const std::vector<T> to_add) {
+void append(std::vector<T> &base, const std::vector<T>& to_add) {
     base.insert(std::end(base), std::begin(to_add), std::end(to_add));
 }
 
