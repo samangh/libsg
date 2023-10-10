8ce421e72a28f83d3f30e0eba4824322aaf3e866
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2023-10-10 17:44:13 +0100

    Add `sg::vector::append` with move semantics

--- include/sg/vector.h
+++ include/sg/vector.h
@@ -9,4 +9,16 @@ void append(std::vector<T> &base, const std::vector<T>& to_add) {
     base.insert(std::end(base), std::begin(to_add), std::end(to_add));
 }
 
+/**
+ * @brief Move contents of one vector to the end of another.
+ *
+ * There is no point in using this for basic types where a move is
+ * equivalnet to copy (e.g. ini, double). But for more complex types
+ * this is useful.
+ */
+template <typename T>
+void append(std::vector<T> &base, std::vector<T>&& to_add) {
+    base.insert(std::end(base), make_move_iterator(to_add.begin()), make_move_iterator(to_add.end()));
+}
+
 } // namespace sg::vector
