9f878bfaed6e78bbe48549733b9c936ed67f60e6
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2023-10-16 18:51:26 +0100

    clang-format `progress.cpp`

--- src/progress.cpp
+++ src/progress.cpp
@@ -1,53 +1,39 @@
 #include "sg/progress.h"
 
-namespace sg
-{
 #include <mutex>
 
-Progress ProgressSource::progress() const
-{
+namespace sg {
+
+Progress ProgressSource::progress() const {
     std::shared_lock lock(m_progress_mutex);
     return m_progress;
 }
 
-void ProgressSource::set_progress(float progress, const std::string &msg)
-{
+void ProgressSource::set_progress(float progress, const std::string &msg) {
     std::unique_lock lock(m_progress_mutex);
     m_progress.progress = progress;
     m_progress.message = msg;
 }
 
-void ProgressSource::set_progress(float progress)
-{
+void ProgressSource::set_progress(float progress) {
     std::unique_lock lock(m_progress_mutex);
     m_progress.progress = progress;
 }
 
-void ProgressSource::set_max_val(float max_val)
-{
+void ProgressSource::set_max_val(float max_val) {
     std::unique_lock lock(m_progress_mutex);
     m_progress.max_val = max_val;
 }
 
-void ProgressSource::set_message(const std::string &msg)
-{
+void ProgressSource::set_message(const std::string &msg) {
     std::unique_lock lock(m_progress_mutex);
     m_progress.message = msg;
 }
 
-void ProgressSource::reset()
-{
-    set_progress(0, "");
-}
+void ProgressSource::reset() { set_progress(0, ""); }
 
-ProgressIndicator::ProgressIndicator(const ProgressSource &source) :m_source(source)
-{
+ProgressIndicator::ProgressIndicator(const ProgressSource &source) : m_source(source) {}
 
-}
+Progress ProgressIndicator::progress() const { return m_source.progress(); }
 
-Progress ProgressIndicator::progress() const
-{
- return m_source.progress();
-}
-
-}
+} // namespace sg
