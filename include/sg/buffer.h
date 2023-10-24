aea010e33c088333ec65ff784a52e27bc34e6e9c
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2023-10-25 00:05:13 +0100

    Add `shared_buffer` and `shared_c_buffer`

--- include/sg/buffer.h
+++ include/sg/buffer.h
@@ -76,9 +76,83 @@ class unique_buffer {
     T *end() const noexcept { return ptr.get() + length; }
 };
 
+template <typename T, typename deleter = std::default_delete<T>> //
+class shared_buffer {
+    std::shared_ptr<T> ptr;
+    size_t length;
+
+  public:
+    /* Default constructor, creates a unique_ptr that owns nothing.*/
+    shared_buffer() noexcept //
+        : length(0){};
+
+    /* Constrcts from bare pointer */
+    shared_buffer(T *_ptr, size_t _length) noexcept //
+        : ptr(std::shared_ptr<T>(_ptr, deleter())), length(_length) {}
+
+    /* Takes ownership of a shared-pointer */
+    shared_buffer(std::shared_ptr<T> _ptr, size_t _length) noexcept //
+        : ptr(_ptr), length(_length) {}
+
+    // Move constrcutors
+    shared_buffer(shared_buffer &&) = default;
+    shared_buffer &operator=(shared_buffer &&data) = default;
+
+    shared_buffer(shared_buffer &) = default;
+    shared_buffer &operator=(shared_buffer &data) = default;
+
+    virtual ~shared_buffer() = default;
+
+    // Implicit cast
+    operator T *() const noexcept { return ptr; }
+
+    /* Return the stored pointer.*/
+    T *get() const noexcept { return ptr.get(); }
+
+    /* Exchange the pointer and the associated length */
+    T *swap(shared_buffer<T, deleter> &other) noexcept {
+        ptr.swap(other.ptr);
+
+        auto other_length = other.length;
+        other.length = length;
+        length = other_length;
+    }
+
+    /* Release ownership of any stored pointer. */
+    void release() noexcept { ptr.release(); }
+
+    /* Returns the number of elements */
+    size_t size() const noexcept { return length; }
+
+    /** Replace the stored pointer.
+     *
+     * The deleter will be invoked if a pointer is already owned.
+     */
+    void reset(T *_ptr, size_t _length) noexcept {
+        ptr.reset(_ptr);
+        this->length = _length;
+    }
+
+    /** Frees the stored pointer.
+     *
+     * The deleter will be invoked if a pointer is already owned.
+     */
+    void reset() noexcept {
+        reset(nullptr, 0);
+    }
+
+    T *begin() const noexcept { return ptr.get(); }
+
+    T *end() const noexcept { return ptr.get() + length; }
+};
+
 /* A version of unique_buffer that uses C-style free() to delete the base pointer */
 template <typename T>
 using unique_c_buffer= unique_buffer<T, deleter_free<T>>;
+
+/* A version of shared_buffer that uses C-style free() to delete the base pointer */
+template <typename T>
+using shared_c_buffer= shared_buffer<T, deleter_free<T>>;
 
 /* Allocates memoery and creates a unique_buffer that uses C-style free as deleter */
 template <typename T>
@@ -94,4 +168,17 @@ unique_c_buffer<T> make_unique_c_buffer(size_t length)
      * but that uses an additional 8-bytes of memory. */
 }
 
+/* Allocates memoery and creates a unique_buffer that uses C-style free as deleter */
+template <typename T>
+shared_c_buffer<T> make_shared_c_buffer(size_t length)
+{
+    T* ptr = (T*)malloc(sizeof(T)*length);
+    return shared_c_buffer<T>(ptr, length);
+
+    /* Note: this could also be done by doing
+     *
+     *     return unique_buffer<T, decltype(&free)>(ptr, length, free);
+     *
+     * but that uses an additional 8-bytes of memory. */
+}
 } // namespace sg
