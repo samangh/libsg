ee35dcf58094e531202a08899aac5a96289421e2
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-02-01 00:28:23 +0000

    data: add comparison for `IChannel` class

--- include/sg/data/channel.h
+++ include/sg/data/channel.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <map>
 #include <sg/buffer.h>
 #include <string>
 
@@ -15,6 +14,29 @@ class IChannelBase {
     virtual void hierarchy(std::vector<std::string>) noexcept = 0;
 
     virtual size_t size() const noexcept = 0;
+
+    virtual bool operator==(const IChannelBase &o) const {
+        return name() == o.name() && hierarchy() == o.hierarchy() && size() == o.size() &&
+               const_data_voidptr() == o.const_data_voidptr();
+    }
+    virtual bool operator<(const IChannelBase &o) const {
+        if (hierarchy() != o.hierarchy())
+            return (hierarchy() < o.hierarchy());
+
+        if (name() != o.name())
+            return (name() < o.name());
+
+        if (size() != o.size())
+            return (size() < o.size());
+
+        if (const_data_voidptr() != o.const_data_voidptr())
+            return (const_data_voidptr() < o.const_data_voidptr());
+
+        return false;
+    };
+
+  protected:
+    virtual const void *const_data_voidptr() const = 0;
 };
 
 template <typename T> class IChannel : public IChannelBase {
@@ -23,6 +45,8 @@ template <typename T> class IChannel : public IChannelBase {
     virtual const T* const_data() const noexcept = 0;
     virtual T operator[](int i) const = 0;
     virtual T &operator[](int i) = 0;
+  protected:
+    virtual const void *const_data_voidptr() const { return (void *)const_data(); };
 };
 
 } // namespace sg::data
