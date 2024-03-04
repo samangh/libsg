2d80759da7c2f96b7a9849a87ca352b2da479d9d
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-03-04 00:47:37 +0000

    math: add `exact_factor_of_two` and `larget_factor_of_two` functions

---
+++ include/sg/math.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+namespace sg::math {
+
+/* Returns the largest EXACT factor of 2 that the input data size is divisble by
+ *
+ * For exmaple:
+ *  - input '8' returns 8
+ *  - input '9' returns 1
+*/
+template <typename T, typename = typename std::enable_if<std::is_unsigned<T>::value>::type>
+T exact_factor_of_two(T number)
+{
+    return number & (~number + 1);
+}
+
+/* Returns the largest factor of 2 that the input data size is divisble by
+ *
+ * For exmaple:
+ *  - input '9' returns 8
+ *  - input '16' returns 16
+*/
+template <typename T, typename = typename std::enable_if<std::is_unsigned<T>::value>::type>
+T largest_power_of_two(T number)
+{
+    number = number| (number>>1);
+    number = number| (number>>2);
+    number = number| (number>>4);
+    number = number| (number>>8);
+    number = number| (number>>16);
+  return number - (number>>1);
+}
+
+}
