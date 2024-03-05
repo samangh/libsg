193fff8afe07312831aa01dafd102b3a015d0991
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-03-05 17:42:19 +0000

    crc: use SSE4.2

--- src/include/internal_crc32c.h
+++ src/include/internal_crc32c.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 /* Optimized hardware assisted CRC32C needs SSE4.2, CRC32 and CLMUL support from the CPU  */
-#if defined(CPU_SUPPORTS_SSE41) && defined(CPU_SUPPORTS_CRC32) && defined(CPU_SUPPORTS_CLMUL)
+#if defined(CPU_SUPPORTS_SSE42) && defined(CPU_SUPPORTS_CRC32) && defined(CPU_SUPPORTS_CLMUL)
     #define HAVE_HARDWARE_CRC32 true
 
 namespace _internal {
@@ -11,6 +11,7 @@ namespace _internal {
 uint32_t crc32c_hardware_amd(const void *M, uint32_t bytes, uint32_t prev);
 uint32_t crc32c_hardware_intel(const void *M, uint32_t bytes, uint32_t prev);
 
+} // namespace _internal
+
 #endif
 
-} // namespace _internal
