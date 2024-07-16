320e6453a0fd0c2e7000a209dfb68bc38db59007
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-07-16 17:36:33 +0100

    Add DefaultCMakePreamble.cmake

---
+++ cmake/DefaultCMakePreamble.cmake
@@ -0,0 +1,52 @@
+# Set this to OFF to build static libraries
+option (BUILD_SHARED_LIBS "Build shared libraries" ON)
+
+if (BUILD_SHARED_LIBS)
+  set(USE_STATIC_LIBS_DEFAULT OFF)
+else()
+  set(USE_STATIC_LIBS_DEFAULT ON)
+endif()
+
+option (USE_STATIC_LIBS "Use external static libs if possible" ${USE_STATIC_LIBS_DEFAULT})
+option (USE_STATIC_RUNTIME "Statically link against the C++ runtime" USE_STATIC_LIBS)
+option (ARCH_NATIVE "Optimise code for current architecture" OFF)
+option (USE_SSE "Enable global use of SSE if possible" ARCH_NATIVE)
+
+if(USE_STATIC_LIBS)
+  include(prioritise_static_libraries)
+  prioritise_static_libraries()
+endif()
+
+##
+## Compile support
+##
+
+# Export compile database for IDEs, needed for QtCreator
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+##
+## Import functions
+##
+
+include(setup_ide_folders)
+include(get_standard_library_name)
+
+# Check for name of standard library, use by common
+include(CheckCXXSourceCompiles)
+get_standard_library_name(STANDARD_LIBRARY)
+
+##
+## Packages
+##
+
+# Setup boost variables, in case any of our projects use it
+include(setup_boost)
+find_package(Sanitizers REQUIRED)
+
+##
+## Policies
+##
+
+# Allows for setting MSVC static runtime
+cmake_policy(SET CMP0091 NEW)
