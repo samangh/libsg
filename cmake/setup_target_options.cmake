006335d8410477be0b0bd7e4da8e5887ee3ade5f
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-07-19 21:49:26 +0100

    Improve static runtime linking in *nix

--- cmake/setup_target_options.cmake
+++ cmake/setup_target_options.cmake
@@ -7,18 +7,18 @@ function(setup_target_options)
 
   target_compile_options(${ARG_TARGET}
     PRIVATE
-      #Warnings
-      $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Wextra>
-      $<$<CXX_COMPILER_ID:MSVC>:/permissive->
+    #Warnings
+    $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Wextra>
+    $<$<CXX_COMPILER_ID:MSVC>:/permissive->
 
-      # Set arch to native (i.e. use all processor flags)
-      $<$<AND:$<BOOL:${ARCH_NATIVE}>,$<NOT:$<CXX_COMPILER_ID:MSVC>>>:-march=native>)
+    # Set arch to native (i.e. use all processor flags)
+    $<$<AND:$<BOOL:${ARCH_NATIVE}>,$<NOT:$<CXX_COMPILER_ID:MSVC>>>:-march=native>)
 
   target_link_libraries(${ARG_TARGET}
     PRIVATE
-      # Enable SSE if
-      $<$<AND:$<BOOL:${USE_SSE}>,$<BOOL:${SSE_SSE42_FOUND}>>:SSE::SSE42>
-      $<$<AND:$<BOOL:${USE_SSE}>,$<BOOL:${SSE_AVX2_FOUND}>>:SSE::AVX2>)
+    # Enable SSE if
+    $<$<AND:$<BOOL:${USE_SSE}>,$<BOOL:${SSE_SSE42_FOUND}>>:SSE::SSE42>
+    $<$<AND:$<BOOL:${USE_SSE}>,$<BOOL:${SSE_AVX2_FOUND}>>:SSE::AVX2>)
 
   # Link against statically to runtime in Windows if enabled
   if (USE_STATIC_RUNTIME)
@@ -26,8 +26,10 @@ function(setup_target_options)
       set_property(TARGET ${ARG_TARGET} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
     else()
       target_link_options(${ARG_TARGET} PRIVATE
-        -static-libstdc++
-        -static-libgcc)
+        # -static-libstdc++ for CXX GCC/Clang/AppleClang
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>>:-static-lib${STANDARD_LIBRARY}>
+        # -static-libgcc for C or CXX, when using GCC/Clang (but not AppleClang)
+        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:GNU>,$<C_COMPILER_ID:GNU>,$<C_COMPILER_ID:GNU>>:-static-libgcc>)
     endif()
   endif()
 
