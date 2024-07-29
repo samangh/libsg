14f45a91134a2428e8d7cbfc3630886689feedf6
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-07-30 00:44:06 +0100

    common: update `sg::data::IChannel`

--- common/include/sg/data/channel.h
+++ common/include/sg/data/channel.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <sg/buffer.h>
+#include <sg/math.h>
+
 #include <string>
 
 namespace sg::data {
@@ -39,13 +41,23 @@ template <typename T> class IChannel : public IChannelBase {
     std::string m_name;
     std::vector<std::string> m_hierarchy;
 
-    // IChannelBase interface
   public:
     std::string name() const noexcept { return m_name; }
     void name(std::string name) noexcept { m_name = name; }
 
     std::vector<std::string> hierarchy() const noexcept { return m_hierarchy; }
     void hierarchy(std::vector<std::string> hierarchy) noexcept { m_hierarchy = hierarchy; }
+
+    virtual T *data() noexcept = 0;
+    virtual const T *data() const noexcept = 0;
+
+    virtual const T& back() const = 0;
+    virtual std::string back_as_string() const = 0;
+
+    virtual const T& operator[](int i) const = 0;
+    virtual T& operator[](int i) = 0;
 };
+
+typedef sg::data::IChannel<double> t_chan_double;
 
 } // namespace sg::data
