3960332ab8b7daab41a40b0dfb24339da7438375
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-08-24 13:40:21 +0100

    Add benchmark testing

--- test/src/file_writer.cpp
+++ test/src/file_writer.cpp
@@ -1,9 +1,13 @@
 #include <cstring>
 #include <sg/file_writer.h>
+#include <sg/file_writer_uv.h>
 
 #include <doctest/doctest.h>
+#include <nanobench.h>
 
 #include <fstream>
+#include <numeric>
+#include <random>
 #include <sstream>
 #include <string>
 #include <cstring>
@@ -11,6 +15,19 @@
 static std::string read_file(std::string path) {
     std::ifstream t(path);
     return std::string((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+}
+
+static std::vector<std::vector<uint64_t>> random_data(std::size_t count, std::size_t bytes)
+{
+    std::vector<std::vector<uint64_t>> vec_data;
+    for (size_t i=0; i< count; ++i)
+    {
+        std::vector<uint64_t> data(bytes / sizeof(uint64_t));
+        std::iota(data.begin(), data.end(), 0);
+        std::shuffle(data.begin(), data.end(), std::mt19937{std::random_device{}()});
+        vec_data.emplace_back(std::move(data));
+    }
+    return vec_data;
 }
 
 TEST_CASE("SG::common sg::file_writer: check writer flushes correctly") {
@@ -96,4 +113,38 @@ TEST_CASE("SG::common sg::file_writer: check bytes_transferred()") {
     writer.write_aync(text);
     writer.stop();
     CHECK_EQ(writer.bytes_transferred(), 4);
+}
+
+TEST_CASE("SG::common file_writer and file_writer_uv performance" ){
+    std::string path = "benchmark-buffer";
+    std::vector<std::vector<uint64_t>> vec_data;
+
+    vec_data =   random_data(100, 2048);
+    SUBCASE("sg::file_writer(..)") {
+        ankerl::nanobench::Bench().minEpochIterations(100).run("sg::file_writer(..)", [&]() {
+            sg::file_writer writer;
+            writer.start(path, nullptr, nullptr, nullptr);
+            for (const auto& buf : vec_data) {
+                auto data_buf = sg::make_shared_c_buffer<std::byte>(buf.size() * sizeof(uint64_t));
+                std::memcpy(data_buf.get(), &buf[0], buf.size() * sizeof(uint64_t));
+                writer.write_aync(std::move(data_buf));
+            }
+            writer.stop();
+        });
+    }
+
+    vec_data =   random_data(100, 2048);
+    SUBCASE("sg::file_writer_uv(..)") {
+        ankerl::nanobench::Bench().minEpochIterations(100).run("sg::file_writer_uv(..)", [&]() {
+            sg::file_writer_uv writer;
+            writer.start(path, nullptr, nullptr, 100);
+            for (const auto& buf : vec_data) {
+                auto data_buf = sg::make_shared_c_buffer<std::byte>(buf.size() * sizeof(uint64_t));
+                std::memcpy(data_buf.get(), &buf[0], buf.size() * sizeof(uint64_t));
+                writer.write(std::move(data_buf));
+            }
+            writer.stop();
+        });
+    }
+
 }
