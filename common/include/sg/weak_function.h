7a5b125514af50c1ed810f8af835014f7969b377
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-08-26 02:44:25 +0100

    common: weak_function tidy up

--- common/include/sg/weak_function.h
+++ common/include/sg/weak_function.h
@@ -14,33 +14,35 @@ namespace sg {
  * This is handly when passing a std::fucntion as a callback, and we want the
  * callback to not be called if the owning object is out of scope.
  *
- * @tparam T    type of oject held by the weakptr
- * @tparam R    return type of the function
- * @tparam ArgT types of function arguments
+ * @tparam PtrT     type of oject held by the weakptr
+ * @tparam ReturnT  return type of the function
+ * @tparam ArgsT    types of function arguments
  */
-template <typename T, typename R, typename... ArgT> class weak_function {
-    std::weak_ptr<T> m_item;
-    std::function<R(ArgT...)> m_func;
+template <typename PtrT, typename ReturnT, typename... ArgsT>
+class weak_function {
+   public:
+    typedef std::weak_ptr<PtrT> ptr_type;
+    typedef std::function<ReturnT(ArgsT...)> func_type;
 
-  public:
-    weak_function(const std::nullptr_t &){}; // Allow cast from nullptr
-    weak_function(std::weak_ptr<T> weakPtr, const std::function<R(ArgT...)>& func)
-        : m_item(weakPtr),
-          m_func(func){};
+    weak_function(const std::nullptr_t &){};  // Allow cast from nullptr, i.e. a weak_function that
+                                              // does nothing
+    weak_function(ptr_type weakPtr, func_type func) : m_item(weakPtr), m_func(std::move(func)){};
 
-    template<typename... U>
-    R operator()(U&& ...args) {
-        if (m_item.lock())
-            return m_func(std::forward<ArgT>(args)...);
-
-        return R();
+    template <typename... U>
+    ReturnT operator()(U &&...args) {
+        if (m_item.lock()) return m_func(std::forward<ArgsT>(args)...);
+        return ReturnT();
     };
     virtual ~weak_function() = default;
+
+   private:
+    ptr_type m_item;
+    func_type m_func;
 };
 
 template <typename R, typename... ArgTypes>
 auto create_weak_function(sg::enable_lifetime_indicator *lifeimeIndicator,
-                          const std::function<R(ArgTypes...)>& func) {
-    return sg::weak_function(lifeimeIndicator->get_lifetime_indicator(), func);
+                          std::function<R(ArgTypes...)> func) {
+    return sg::weak_function(lifeimeIndicator->get_lifetime_indicator(), std::move(func));
 }
-} // namespace sg
+}  // namespace sg
