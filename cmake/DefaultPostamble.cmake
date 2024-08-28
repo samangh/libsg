b0f3366dc112fdea0141ef0c5409688cfc0a2eee
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-08-28 23:31:20 +0100

    cmake: format DefaultPostamble.cmake

--- cmake/DefaultPostamble.cmake
+++ cmake/DefaultPostamble.cmake
@@ -7,7 +7,7 @@ get_all_targets(_TARGETS)
 
 # Link against statically to runtime if enabled.
 #
-# It's better to use ${get_all_targets}, as that also pull in any
+# It's better to use ${get_all_targets}, as that also pulls in any
 # subprojects that we might be using (e.g. nanobench)
 
 if (USE_STATIC_RUNTIME)
@@ -17,9 +17,15 @@ if (USE_STATIC_RUNTIME)
     else()
       target_link_options(${TARGET} PRIVATE
         # -static-libstdc++ for CXX GCC/Clang/AppleClang
-        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>>:-static-lib${STANDARD_LIBRARY}>
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>, $<OR:$<CXX_COMPILER_ID:Clang>,
+                                              $<CXX_COMPILER_ID:AppleClang>,
+                                              $<CXX_COMPILER_ID:GNU>>>:-static-lib${STANDARD_LIBRARY}>
+
         # -static-libgcc for C or CXX, when using GCC/Clang (but not AppleClang)
-        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:GNU>,$<C_COMPILER_ID:GNU>,$<C_COMPILER_ID:GNU>>:-static-libgcc>)
+        $<$<OR:$<CXX_COMPILER_ID:Clang>,
+               $<CXX_COMPILER_ID:GNU>,
+               $<C_COMPILER_ID:GNU>,
+               $<C_COMPILER_ID:GNU>>:-static-libgcc>)
     endif()
   endforeach()
 endif()
