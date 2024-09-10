54fc7e8833e09ab43aa3dc749cb89087308ff16c
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-09-10 23:01:57 +0100

    Tidy up `sg::enumeration`

--- common/include/sg/enumeration.h
+++ common/include/sg/enumeration.h
@@ -4,33 +4,33 @@
 
 namespace sg::enumeration {
 
-template<typename enumT, typename std::enable_if<std::is_enum<enumT>::value>::type* = nullptr>
-[[nodiscard]] inline constexpr enumT operator | (enumT lhs, enumT rhs)
-{
+template <typename enumT>
+    requires std::is_enum_v<enumT>
+[[nodiscard]] constexpr enumT operator|(enumT lhs, enumT rhs) {
     using T = std::underlying_type_t<enumT>;
     return static_cast<enumT>(static_cast<T>(lhs) | static_cast<T>(rhs));
 }
 
-template<typename enumT, typename std::enable_if<std::is_enum<enumT>::value>::type* = nullptr>
-[[nodiscard]] inline constexpr  enumT& operator |= (enumT& lhs, enumT rhs)
-{
+template <typename enumT>
+    requires std::is_enum_v<enumT>
+[[nodiscard]] constexpr enumT& operator|=(enumT& lhs, enumT rhs) {
     lhs = lhs | rhs;
     return lhs;
 }
 
-template<typename enumT, typename std::enable_if<std::is_enum<enumT>::value>::type* = nullptr>
-[[nodiscard]] inline constexpr enumT operator&(enumT lhs, enumT rhs)
-{
+template <typename enumT>
+    requires std::is_enum_v<enumT>
+[[nodiscard]] constexpr enumT operator&(enumT lhs, enumT rhs) {
     using T = std::underlying_type_t<enumT>;
-    return static_cast<enumT> (static_cast<T>(lhs) & static_cast<T>(rhs));
+    return static_cast<enumT>(static_cast<T>(lhs) & static_cast<T>(rhs));
 }
 
-
-template<typename enumT, typename std::enable_if<std::is_enum<enumT>::value>::type* = nullptr>
-[[nodiscard]] inline bool contains(enumT enumeration, enumT enum_to_check)
-{
+template <typename enumT>
+    requires std::is_enum_v<enumT>
+[[nodiscard]] bool contains(enumT enumeration, enumT enum_to_check) {
     using T = std::underlying_type_t<enumT>;
-    return (static_cast<T>(enumeration) & static_cast<T>(enum_to_check)) == static_cast<T>(enum_to_check);
+    return (static_cast<T>(enumeration) & static_cast<T>(enum_to_check)) ==
+           static_cast<T>(enum_to_check);
 }
 
-}
+}  // namespace sg::enumeration
