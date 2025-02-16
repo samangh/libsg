77840ede13172978c4901a16828d346514ff7bce
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-02-16 16:16:32 +0000

    vector: add `vector_no_initialise` alias

--- common/include/sg/vector.h
+++ common/include/sg/vector.h
@@ -31,4 +31,36 @@ template <typename T> void append(std::vector<T> &base, std::vector<T> &&to_add)
         std::end(base), make_move_iterator(to_add.begin()), make_move_iterator(to_add.end()));
 }
 
+/**
+ * @brief Allocator adaptor that interposes construct() calls to
+ *        convert value initialization into default initialization.
+ *
+ *        Pass to std::vector, to create a vector that does not initialise
+ *        the values.
+ */
+template <typename T, typename A = std::allocator<T>> class default_init_allocator : public A {
+    /* From:
+     * https://stackoverflow.com/questions/21028299/is-this-behavior-of-vectorresizesize-type-n-under-c11-and-boost-container/21028912
+     */
+    typedef std::allocator_traits<A> a_t;
+
+  public:
+    template <typename U> struct rebind {
+        using other = default_init_allocator<U, typename a_t::template rebind_alloc<U>>;
+    };
+
+    using A::A;
+
+    template <typename U>
+    void construct(U* ptr) noexcept(std::is_nothrow_default_constructible<U>::value) {
+        ::new (static_cast<void*>(ptr)) U;
+    }
+    template <typename U, typename... Args> void construct(U* ptr, Args&&... args) {
+        a_t::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
+    }
+};
+
+/* a std::vector<T> that does not initialise new members when doing resize(...) */
+template <typename T> using vector_no_initialise = std::vector<T, default_init_allocator<T>>;
+
 } // namespace sg::vector
