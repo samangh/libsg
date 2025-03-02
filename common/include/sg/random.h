eb242a47ea78c2f8f5939bc81f5cfa8b16866a08
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-03-02 00:59:28 +0000

    random: add `sg::random::generate(...)` function

---
+++ common/include/sg/random.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+namespace sg::random {
+
+/* generate a vector of data (in range 0 to count, shuffled)*/
+template<std::integral T>
+std::vector<T> genrate(std::size_t count) {
+    std::vector<T> data(count);
+    std::iota(data.begin(), data.end(), static_cast<T>(0));
+    std::shuffle(data.begin(), data.end(), std::mt19937{std::random_device{}()});
+    return data;
+}
+
+} // namespace sg
