16ddbc475e0def7a0d80e8934978877e7228cfe3
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-03-09 23:53:39 +0000

    Submodule update

--- common/src/include/crc32c_defs.h
+++ common/src/include/crc32c_defs.h
@@ -2,8 +2,8 @@
 
 #include <sg/environment.h>
 
-/* Optimized hardware assisted CRC32C needs SSE4.2, CRC32 and CLMUL support from the CPU  */
-#if defined(CPU_SUPPORTS_SSE42) && defined(CPU_SUPPORTS_CRC32) && defined(CPU_SUPPORTS_CLMUL)
+/* Optimized hardware assisted CRC32C needs SSE4.2 and CLMUL support from the CPU  */
+#if defined(CPU_SUPPORTS_SSE42) && defined(CPU_SUPPORTS_CLMUL)
     #define HAVE_HARDWARE_CRC32 true
 #endif
 
