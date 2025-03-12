4935d965fcaf856c6b2b16c6b6c0d38c46d384f6
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-03-12 10:51:05 +0000

    crc: simplify 32-bit hardware calculation

--- common/src/include/crc32c_hardware_32bit.h
+++ common/src/include/crc32c_hardware_32bit.h
@@ -6,33 +6,33 @@
 
 namespace {
 
-inline uint32_t crc32c_32bit_1byte(const void *data,
-                            std::size_t no_of_bytes,
-                            uint32_t remainder) {
-    auto R = remainder;
-    auto M = (const uint8_t*)data;
-    for (uint32_t i = 0; i < no_of_bytes; ++i) {
-        R = _mm_crc32_u8(R, M[i]);
-    }
-    return R;
-}
-
-
-
 /* Hardware-assited CRC-32c
  *
  * Basic 32-bit version
- */
+*/
 inline uint32_t crc32c_hardware_32bit(const void * data, uint32_t no_of_bytes, uint32_t prev){
     auto R = prev;
     auto M = (const uint8_t*)data;
-    uint32_t i = 0;
-    while (i + 4 < no_of_bytes) {
-        R = _mm_crc32_u32(R, *((uint32_t*)&M[i]));
-        i += 4;
-    }
 
-    return crc32c_32bit_1byte(&M[i], no_of_bytes - i, R);
+    while (no_of_bytes >=4) {
+        R = _mm_crc32_u32(R, *(uint32_t*)M);
+        no_of_bytes -=4;
+        M += 4;
+    };
+
+    if (no_of_bytes>=2) {
+        R = _mm_crc32_u16(R, *(uint16_t*)M);
+        no_of_bytes -=2;
+        M += 2;
+    };
+
+    if (no_of_bytes==1) {
+        R = _mm_crc32_u8(R, *M);
+        no_of_bytes -=1;
+        M +=1;
+    };
+
+    return R;
 }
 
 }
