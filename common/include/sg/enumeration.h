e5eb651bd5483ad6646a8765c397eb8684394173
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-04-08 17:02:03 +0100

    ernumeration: add `helper` class

--- common/include/sg/enumeration.h
+++ common/include/sg/enumeration.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <fmt/format.h>
+#include <map>
+#include <string>
 #include <type_traits>
 
 namespace sg::enumeration {
@@ -44,6 +47,43 @@ template <typename enumT, typename T = std::underlying_type_t<enumT>>
 [[nodiscard]] enumT from_underlying_value(T underlying_value) {
     return static_cast<enumT>(underlying_value);
 }
+
+
+template<typename T>
+    requires(std::is_enum_v<T>)
+class helper {
+  public:
+    helper(){};
+    helper(std::initializer_list<std::pair<T, std::string>> list) {
+        for (const auto& [val, name] : list)
+            add(val, name);
+    }
+
+    virtual ~helper() =default;
+
+    void add(T enumVal, std::string name ){
+        if (m_name_map.contains(name))
+            throw std::logic_error(fmt::format("duplicate enum value name {} specific", name));
+
+        if (m_value_map.contains(enumVal))
+            throw std::logic_error(fmt::format("duplicate enum value {}, with name {} specificed", sg::enumeration::underlying_value(enumVal), name));
+
+        m_name_map.emplace(name,enumVal);
+        m_value_map.emplace(enumVal, name);
+    }
+
+    [[nodiscard]] std::string to_string(T value) const{
+        return m_value_map.at(value);
+    }
+
+    [[nodiscard]] T to_enum(std::string name) {
+        return m_name_map.at(name);
+    }
+
+  private:
+    std::map<std::string, T> m_name_map;
+    std::map<T, std::string> m_value_map;
+};
 
 
 }  // namespace sg::enumeration
