c7a98f17f99cfa398eb1f566c5fc03510e0476bd
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-05-09 23:32:19 +0100

    tcp_listener: remove deprecated warning

--- common/src/tcp_listener.cpp
+++ common/src/tcp_listener.cpp
@@ -1,3 +1,9 @@
+#ifdef _MSC_VER
+    #pragma warning(disable : 4996)
+#else
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 #include "sg/tcp_listener.h"
 #include "sg/buffer.h"
 #include "sg/libuv_wrapper.h"
