ca21618d94870f212105d1fcf001e512c1705f24
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-09-27 17:57:19 +0100

    fixup! string: only modify LCTYPE for `set_global_utf8_codepage`

--- common/src/string.cpp
+++ common/src/string.cpp
@@ -29,9 +29,24 @@ bool localname_has_utf8(const std::string& input) {
 }
 
 void set_global_locale_ctype(const std::string& ctype) {
+    /* Note:
+     *
+     * The C locale and the C++ locales are mostly unrelated. However, making a C++ locale
+     * object global via locale::global() affects the global C locale and results in a call to
+     * setlocale(). When this happens, locale-sensitive C functions called from within a C++ program
+     * will use the global C++ locale.
+     *
+     * There is no way to affect the C++ locale from within a C program.
+     *
+     * See https://www.chem.kuleuven.be/peeters/manuals/pgi/doc/pgC++_lib/stdlibug/sta_9169.htm
+     */
+
     // create a copy of the default locale, the copy the CTYPE part from the user environment
     std::locale newLocale(std::locale(), ctype, std::locale::ctype);
     std::locale::global(newLocale);
+
+    // On macOS, setting C++ ctype locale seems to not propagate LC_CTYPE
+    std::setlocale(LC_CTYPE, ctype.c_str());
 }
 } // namespace
 
@@ -54,15 +69,12 @@ std::wstring to_wstring(const char* input) {
 std::wstring to_wstring(const std::string& input) { return to_wstring(input.c_str()); }
 
 void set_global_utf8_codepage() {
-    std::locale e(std::locale(""), "en_GB.utf8", std::locale::ctype);
-    std::locale::global(e);
-
     // If default LC_CTYPE uses UTF8 codepage, then exit
     if (localname_has_utf8(std::setlocale(LC_CTYPE, nullptr)))
         return;
 
     // If the user-environment uses UTF8 codepage, create a copy of the default locale, then
-    // copy the CTYPE part from the user environment
+    // copy the CTYPE part from the user-environment
     if (localname_has_utf8(std::locale("").name()))
         TRY_EXIT_ON_SUCCESS(set_global_locale_ctype(""));
 
