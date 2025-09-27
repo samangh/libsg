43edce5280f90a26d6fe252eb282ba5684190b34
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-09-27 17:30:17 +0100

    string: only modify LCTYPE for `set_global_utf8_codepage`

--- common/src/string.cpp
+++ common/src/string.cpp
@@ -6,8 +6,6 @@
 #include <stdexcept>
 #include <string>
 
-namespace sg::common {
-
 /* wide-string functions return -1 on erro */
 #define THROW_ON_ERROR(fn)                                                                         \
     do {                                                                                           \
@@ -15,6 +13,29 @@ namespace sg::common {
         if (err == static_cast<size_t>(-1))                                                        \
             throw std::runtime_error(strerror(errno));                                             \
     } while (0)
+
+#define TRY_EXIT_ON_SUCCESS(fn)                                                                    \
+    do {                                                                                           \
+        try {                                                                                      \
+            (fn);                                                                                  \
+            return;                                                                                \
+        } catch (...) {}                                                                           \
+    } while (0)
+
+namespace {
+bool localname_has_utf8(const std::string& input) {
+    return input.ends_with("UTF8") || input.ends_with("UTF-8") || input.ends_with("utf8") ||
+           input.ends_with("utf-8") || input.ends_with("Utf8") || input.ends_with("Utf-8");
+}
+
+void set_global_locale_ctype(const std::string& ctype) {
+    // create a copy of the default locale, the copy the CTYPE part from the user environment
+    std::locale newLocale(std::locale(), ctype, std::locale::ctype);
+    std::locale::global(newLocale);
+}
+} // namespace
+
+namespace sg::common {
 
 std::wstring to_wstring(const char* input) {
     std::mbstate_t state{};
@@ -33,23 +54,26 @@ std::wstring to_wstring(const char* input) {
 std::wstring to_wstring(const std::string& input) { return to_wstring(input.c_str()); }
 
 void set_global_utf8_codepage() {
+    std::locale e(std::locale(""), "en_GB.utf8", std::locale::ctype);
+    std::locale::global(e);
+
     // If default LC_CTYPE uses UTF8 codepage, then exit
-    if(std::string cType = std::setlocale(LC_CTYPE, NULL);  !cType.empty() && cType.back() == '8')
+    if (localname_has_utf8(std::setlocale(LC_CTYPE, nullptr)))
         return;
 
-    // If the user-environment uses UTF8 codepage, use that
-    if (const auto local = std::locale("").name(); !local.empty() && local.back() == '8') {
-        std::locale::global(std::locale("C.utf8"));
-        return;
-    }
+    // If the user-environment uses UTF8 codepage, create a copy of the default locale, then
+    // copy the CTYPE part from the user environment
+    if (localname_has_utf8(std::locale("").name()))
+        TRY_EXIT_ON_SUCCESS(set_global_locale_ctype(""));
 
-//FYI: macos uses utf8 encodig by deffault, no need to change that
+    // Try different possibilities one by one until one works
 #if defined(_WIN32)
-    // Use current locale, just change to UTF8 codepage
-    std::locale::global(std::locale(".utf8"));
-#elif defined(__linux__)
-    std::locale::global(std::locale("C.utf8"));
+    // Use current locale, just change to UTF8 codepage (Windows only)
+    TRY_EXIT_ON_SUCCESS(set_global_locale_ctype(".utf8"));
 #endif
+    TRY_EXIT_ON_SUCCESS(set_global_locale_ctype("C.utf8"));
+    TRY_EXIT_ON_SUCCESS(set_global_locale_ctype("en_US.utf8"));
+    TRY_EXIT_ON_SUCCESS(set_global_locale_ctype("en_GB.utf8"));
 }
 
 } // namespace sg::common
