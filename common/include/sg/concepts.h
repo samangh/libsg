dea5ca3be416d6f255fa52a23a1b5ddca5c04214
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-12-09 23:47:12 +0000

    concepts: add `is_time_point_v` concept

--- common/include/sg/concepts.h
+++ common/include/sg/concepts.h
@@ -2,10 +2,15 @@
 
 #include <concepts>
 #include <optional>
+#include <chrono>
 
 namespace sg::concepts {
 
 template <typename T>
 concept is_optional = std::is_same_v<T, std::optional<typename T::value_type>>;
+
+template <typename T>
+concept is_time_point_v =
+    std::is_same_v<T, std::chrono::time_point<typename T::clock, typename T::duration>>;
 
 } // namespace sg::concepts
