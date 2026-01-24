649318de9e950f619d40db110b97a3dd6fb7fe06
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2026-01-24 00:50:39 +0000

    tcp_native: simply functions

--- common/include/sg/tcp_native.h
+++ common/include/sg/tcp_native.h
@@ -1,17 +1,29 @@
 #pragma once
-#include <cstdint>
 #include <sg/export/common.h>
+
+#if defined(_WIN32)
+    #include <Winsock2.h>
+#endif
 
 namespace sg::net::native {
 
 #if defined(_WIN32)
-typedef int SOCKET;
+typedef SOCKET socket_t;
 #else
 typedef int socket_t;
 #endif
 
-void SG_COMMON_EXPORT set_keepalive(socket_t nativeHandle, bool enableKeepAlive,
-                                    std::uint32_t idleSec, uint32_t intervalSec, uint32_t count);
+/** Enables/disables TCP keepalive. If the keep-alive set to be disabled then the additional
+ * parameters are ignored.
+ *
+ * @param nativeHandle Native platform socket handle
+ * @param enableKeepAlive Set true to enable keepalive, false to disable
+ * @param idleSec Number of seconds of no-activity before a keepalive packet is sent
+ * @param intervalSec Interal between each keep alive packet
+ * @param count Number of keepalive packets to send before terminating the connection
+ */
+void SG_COMMON_EXPORT set_keepalive(socket_t nativeHandle, bool enableKeepAlive, unsigned idleSec,
+                                    unsigned intervalSec, unsigned count);
 
-void SG_COMMON_EXPORT set_timeout(socket_t nativeHandle, uint32_t timeoutMSec);
+void SG_COMMON_EXPORT set_timeout(socket_t nativeHandle, unsigned timeoutMSec);
 }
--- common/src/tcp_native.cpp
+++ common/src/tcp_native.cpp
@@ -1,54 +1,73 @@
 #include "sg/tcp_native.h"
-#include "sg/tcp_session.h"
+
+#include <limits>
+#include <stdexcept>
 
 #if defined(_WIN32)
-#include <Winsock2.h>
-#include <Mswsock.h>
+    #include <Mswsock.h>
+    #include <Winsock2.h>
+    #include <Ws2tcpip.h>
+#else
+    #include <netinet/in.h>
+    #include <netinet/ip.h>
+    #include <netinet/tcp.h>
+    #include <sys/socket.h>
 #endif
 
 namespace sg::net::native {
 
-void set_keepalive(socket_t nativeHandle, bool enableKeepAlive, uint32_t idleSec, uint32_t intervalSec,
-                   uint32_t count) {
+void set_keepalive(socket_t nativeHandle, bool enableKeepAlive, unsigned idleSec,
+                   unsigned intervalSec, unsigned count) {
 
     /* note:
-     *
      *  - in boost could have used teh set_option(..) to enable keep-alive, but that does not
      *    modify the interval values, etc
-     *  - values have to be passed as `const char *` in Windows
+     *  - values have to be passed as `const char *` in Windows, but `void *` in Linux/Apple. As
+     *    `void *` can automatically take `const char *`, let's use `const char *` everywhere
+     *  - Windows actually can take `uint32_t` here, but let's just limit to INTMAX for sake of
+     *    consistency
      */
 
+    // You can cast `unsinged *` to `int *` safely as long as it's less than INTMAX, as otherwise
+    // the int* becomes -ve.
+    //
+    // The (std::numeric_limits<int>::max)() syntax prevents confusion with the `max` macro defined
+    // in windows headers
+    if (idleSec > (std::numeric_limits<int>::max)() ||
+        intervalSec > (std::numeric_limits<int>::max)() ||
+        count > (std::numeric_limits<int>::max)())
+        throw std::invalid_argument("keepalive parameters are out of range");
+
     int keepAliveInt = enableKeepAlive;
-    #if defined(_WIN32)
-    setsockopt(nativeHandle, SOL_SOCKET, SO_KEEPALIVE, (const char*)&keepAliveInt, sizeof(keepAliveInt));
-    #else
-    setsockopt(nativeHandle, SOL_SOCKET, SO_KEEPALIVE, &keepAliveInt, sizeof(keepAliveInt));
-    #endif
+    setsockopt(nativeHandle, SOL_SOCKET, SO_KEEPALIVE, (const char*)&keepAliveInt,
+               sizeof(keepAliveInt));
 
     if (enableKeepAlive) {
-        #if defined(__APPLE__)
-        //Apple only allow setting idle time
+#if defined(__APPLE__)
+        // Apple only allow setting idle time, cast others to void to prevent compiler warning about
+        // unused variables
+        (void)intervalSec;
+        (void)count;
         setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPALIVE, &idleSec, sizeof(idleSec));
-        #elif defined(_WIN32)
+#else
         setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPIDLE, (const char*)&idleSec, sizeof(idleSec));
-        setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPINTVL, (const char*)&intervalSec, sizeof(intervalSec));
+        setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPINTVL, (const char*)&intervalSec,
+                   sizeof(intervalSec));
         setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPCNT, (const char*)&count, sizeof(count));
-        #else
-        setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPIDLE, &idleSec, sizeof(idleSec));
-        setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPINTVL, &intervalSec, sizeof(intervalSec));
-        setsockopt(nativeHandle, IPPROTO_TCP, TCP_KEEPCNT, &count, sizeof(count));
-        #endif
+#endif
     }
 }
 
-void set_timeout(socket_t nativeHandle, uint32_t timeoutMSec) {
-    #if defined(_WIN32)
-    setsockopt(nativeHandle, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeoutMSec, sizeof(timeoutMSec));
-    setsockopt(nativeHandle, SOL_SOCKET, SO_SNDTIMEO, (const char*)&timeoutMSec, sizeof(timeoutMSec));
-    #else
-    const timeval tv{.tv_sec = timeoutMSec / 1000, .tv_usec = (timeoutMSec % 1000) * 1000} ;
+void set_timeout(socket_t nativeHandle, unsigned timeoutMSec) {
+#if defined(_WIN32)
+    setsockopt(nativeHandle, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeoutMSec,
+               sizeof(timeoutMSec));
+    setsockopt(nativeHandle, SOL_SOCKET, SO_SNDTIMEO, (const char*)&timeoutMSec,
+               sizeof(timeoutMSec));
+#else
+    const timeval tv{.tv_sec = timeoutMSec / 1000, .tv_usec = (timeoutMSec % 1000) * 1000};
     setsockopt(nativeHandle, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
     setsockopt(nativeHandle, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
-    #endif
+#endif
 }
-}
+} // namespace sg::net::native
