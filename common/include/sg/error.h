4f67f1097a901372b9a74e14e5d4cb661580886a
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2026-01-25 10:55:26 +0000

    error: add comments

--- common/include/sg/error.h
+++ common/include/sg/error.h
@@ -37,7 +37,17 @@ namespace sg::error {
 #if defined(_WIN32)
 typedef uint32_t native_error_t;
 std::string SG_COMMON_EXPORT get_windows_error_message(native_error_t errorID);
+
+/** Returns the last Windows error via <tt>GetLastError()</tt>.
+ *
+ * This is a separate function so that the Windows headers are not included when you include this
+ * header. **/
 native_error_t SG_COMMON_EXPORT get_windows_last_error();
+
+/** Returns the last Windows socket error via <tt>WSAGetLastError()</tt>.
+ *
+ * This is a separate function so that the Windows headers are not included when you include this
+ * header. **/
 native_error_t SG_COMMON_EXPORT get_windows_last_socket_error();
 #endif
 
