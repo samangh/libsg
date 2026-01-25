f596a53ba54a1b9fe381384b205fe0f80936489a
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2026-01-25 01:46:23 +0000

    error: use Windows headers in source file, not header

--- common/include/sg/error.h
+++ common/include/sg/error.h
@@ -3,23 +3,22 @@
 
 #include <stdexcept>
 #include <string>
+#include <cstdint>
 
 /* get error message from errno */
 #if defined(_WIN32)
-    #define WIN32_LEAN_AND_MEAN
-    #define VC_EXTRALEAN
-    #include <Windows.h>
-    #include <Winsock2.h>
-
     #define THROW_ON_ERRORNO(fn)                                                                   \
         do {                                                                                       \
             if ((fn) == 0) /* On Windows, most function return 0 on error */                       \
-                throw std::runtime_error(sg::error::get_windows_error_message(GetLastError()));    \
+                throw std::runtime_error(                                                          \
+                    sg::error::get_windows_error_message(sg::error::get_windows_last_error()));    \
         } while (0)
 
-    #define THROW_ON_ERRORNO_SOCKET(fn) do {                                                       \
-        if ((fn) == SOCKET_ERROR)                                                                  \
-                throw std::runtime_error(sg::error::get_windows_error_message(WSAGetLastError())); \
+    #define THROW_ON_ERRORNO_SOCKET(fn)                                                            \
+        do {                                                                                       \
+            if ((fn) == SOCKET_ERROR)                                                              \
+                throw std::runtime_error(sg::error::get_windows_error_message(                     \
+                    sg::error::get_windows_last_socket_error()));                                  \
         } while (0)
 #else
     #include <cerrno>
@@ -36,7 +35,10 @@
 namespace sg::error {
 
 #if defined(_WIN32)
-std::string SG_COMMON_EXPORT get_windows_error_message(DWORD errorID);
+typedef uint32_t native_error_t;
+std::string SG_COMMON_EXPORT get_windows_error_message(native_error_t errorID);
+native_error_t SG_COMMON_EXPORT get_windows_last_error();
+native_error_t SG_COMMON_EXPORT get_windows_last_socket_error();
 #endif
 
 } // namespace sg::windows
