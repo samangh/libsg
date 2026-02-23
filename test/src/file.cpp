926b62bd4f2145d4f8af990a1287583e6a38e6be
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2026-02-23 21:46:11 +0000

    file: rename test sections

--- test/src/file.cpp
+++ test/src/file.cpp
@@ -9,7 +9,7 @@ TEST_CASE("sg::file read(...)", "[sg::file]") {
     std::filesystem::path path = "test.txt";
     std::string content = "test string";
 
-    SECTION("returning contents in a new buffer") {
+    SECTION("... returning contents in a new buffer") {
         std::ofstream myfile;
         myfile.open(path, std::ios::out | std::ios::trunc);
         myfile << content;
@@ -20,7 +20,7 @@ TEST_CASE("sg::file read(...)", "[sg::file]") {
         REQUIRE(redbackStr==content);
     }
 
-    SECTION("saving contents to existing buffer") {
+    SECTION("... saving contents to existing buffer") {
         std::ofstream myfile;
         myfile.open (path, std::ios::out | std::ios::trunc);
         myfile << content;
@@ -33,7 +33,7 @@ TEST_CASE("sg::file read(...)", "[sg::file]") {
         REQUIRE(redbackStr==content);
     }
 
-    SECTION("check opening in wrong mode throws exception") {
+    SECTION("... check opening in wrong mode throws exception") {
         auto buffer = sg::make_shared_c_buffer<std::byte>(content.size());
         REQUIRE_THROWS(sg::common::file::read(path, buffer.get(), buffer.size(), std::ios::out));
     }
